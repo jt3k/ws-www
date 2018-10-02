78c77012164a2299bbd529b57a3473b79022cc49
Andrey Gurtovoy <iyntx@ya.ru>
2018-10-02 19:52:11 +0300

    feat(app): hot reload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./App.js";
 
-render(<App />, document.body);
+let root = document.getElementById("app");
+function init() {
+	let App = require("./App.js").default;
+	render(<App />, root);
+}
+
+if (module.hot) {
+	module.hot.accept("./App.js", init);
+}
+
+init();
